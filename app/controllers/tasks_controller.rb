03f5bdb7409e009c78bdd2fb2eb904bc0bbff085
hideto.kashiyama <grant620523@gmail.com>
2019-09-14 07:58:59 +0000

    add_login5

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+ # before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:destroy, :show, :edit, :update]
   
   def index
@@ -71,9 +71,11 @@ class TasksController < ApplicationController
    
   private
 
-  def set_task
-    @task = Task.find_by(user_id: current_user.id)
-  end
+  #def set_task
+    #@task = Task.find_by(user_id: current_user.id)
+  #end
+  #orrect_userメソッドがset_taskメソッドを兼務しているのでset_taskメソッドは不要
+  
   # Strong Parameter
   
   def task_params
